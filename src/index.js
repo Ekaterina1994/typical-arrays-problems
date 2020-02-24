c5f1daeda637ff521aa25f4b676d2a82269d6233
Ekaterina1994 <katishteno@mail.ru>
2020-02-24 21:23:23 +0300

    feat: task is ready

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length == 0) return 0;
+    let a = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < a) {
+            a = array[i];
+        }
+    }
+    return a;
 }
 
 exports.max = function max (array) {
-  return 0;
-}
+    if (!array || array.length == 0) return 0;
+    let b = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > b) {
+            b = array[i];
+        }
+    }
+    return b;
+  }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length == 0) return 0;
+  var sum = array[0];
+  for(let i = 1; i < array.length; i++){
+      sum += array[i];
+      }
+      return sum / array.length;
+
 }
